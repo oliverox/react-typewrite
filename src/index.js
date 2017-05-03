d948d62a634a7a363f43a88ae750a23c7726abee
Oliver Oxenham <ooxenham@stubhub.com>
2017-05-03 09:03:19 -0700

    basic animation with setTimeout

--- src/index.js
+++ src/index.js
@@ -12,18 +12,34 @@ class Typewrite extends Component {
       text: ''
     }
   }
-  
-  componentDidMount() {                                                                    
-    const {initialDelay, children} = this.props;
-    window.setTimeout(function() {
-      this.setState({
-        text: `${children[0]}`
-      })
-    }.bind(this), initialDelay);
+
+  componentDidMount() {
+    const {initialDelay, typingDelay, children} = this.props;
+    let delay = initialDelay;
+
+    const animate = (char) => {
+      return function() {
+        this.appendCharacter(char);
+      }.bind(this)
+    }
+
+    for (let char of children) {
+      console.log('character:', char);
+      window.setTimeout(animate(char), delay);
+      delay = typingDelay;
+    }
   }
-  
+
+  appendCharacter(char) {
+    const currentText = this.state.text;
+    this.setState({
+      text: `${currentText}${char}`
+    });
+    this.forceUpdate();
+  }
+
   render() {
-    const {htmlTag, initialDelay, children, ...rest} = this.props;
+    const {htmlTag, initialDelay, typingDelay, children, ...rest} = this.props;
     const Tag = htmlTag;
     return(
       <Tag style={styles} {...rest}>
@@ -39,18 +55,20 @@ class Typewrite extends Component {
 
 Typewrite.defaultProps = {
   htmlTag: 'span',
-  initialDelay: 300
+  initialDelay: 300,
+  typingDelay: 2000,
 };
 
 Typewrite.propTypes = {
   htmlTag: PropTypes.oneOf([
-    'span', 'p', 'div', 
-    'h1', 'h2', 'h3', 
+    'span', 'p', 'div',
+    'h1', 'h2', 'h3',
     'h4', 'h5', 'h6'
   ]),
   className: PropTypes.string,
   children: PropTypes.string.isRequired,
-  initialDelay: PropTypes.number
+  initialDelay: PropTypes.number,
+  typingDelay: PropTypes.number,
 };
 
 export default Typewrite;
